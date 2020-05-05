aacc39757dfcee16ed3f3ffc6ba71937a4fc54b4
hyunsooklm <hyunsooklm@naver.com>
2020-05-05 22:52:29 +0900

    mysql로 상세페이지 구현

--- algo.js
+++ algo.js
@@ -1,31 +1,27 @@
-const INF=210000;
-const ver=6;
-
-
-    var graph=[
-        [0,5,4,0,0,0],
-        [5,0,2,7,0,0],
-        [4,2,0,6,11,0],
-        [0,7,6,0,3,8],
-        [0,0,11,3,0,8],
-        [0,0,0,8,8,0],
-    ]
-    var distance=[];
-    var sum=0;
-    var selected={};
-    selected[0]=0;
-    distance[0]=0;
-    for(var i=1;i<ver;i++){
-        distance[i]=INF;
-        for(var k in selected){
-            var k=parseInt(k);
-            if(selected.i==undefined&&0<graph[i][k]&&graph[i][k]<INF&&distance[i]>graph[i][k]){
-                const r=i;
-                distance[i]=graph[i][k];
-                selected.r=i;
-                sum+=distance[i];
-            }
-        }
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'kimhs1019@',//mysql -uroot -p 
+  database : 'hyunsoo_db' //use some databases
+});
+ 
+connection.connect(function(err){
+    if(err){
+        console.error('error connecting: '+err.stack);
+        return;
     }
-    console.log(sum);
+    console.log('connected as id '+connection.threadId);
+}); //연결
+ 
+connection.query('SELECT * from topic', function (error, results, fields) { //query날리기 result에 결과값 나옴
+  if (error) {
+      console.log(error);
+  }
+  else{
+    console.log('The solution is: ', results);
 
+  }
+});
+ 
+connection.end();//exit
--- app.js
+++ app.js
@@ -4,10 +4,22 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-var Template=require('./lib/Template.js');
-var isEmpty=require('./lib/Empty.js');
-var sanitizehtml=require('sanitize-html');
-
+var Template = require('./lib/Template.js');
+var isEmpty = require('./lib/Empty.js');
+var sanitizehtml = require('sanitize-html');
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'kimhs1019@',
+  database: 'hyunsoo_db'
+});
+db.connect(function (err) {
+  if (err) {
+    console.error('error connectiong' + err.stack);
+    return;
+  }
+})
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
@@ -16,62 +28,92 @@ var app = http.createServer(function (request, response) {
   var title = queryData.id;
   var html = "";
   var description = "";
-  var filelists = [];
+  var resultlist = [];
   var body = '';
-  console.log(queryData);
   if (pathname === '/') {
     if (queryData.id == undefined) {
-      var path=url.parse(_url,true).path;
-      if(path!='/'){
+      var path = url.parse(_url, true).path;
+      if (path != '/') {
         response.writeHead(404);
         response.end('Not found!');
       }
-      console.log(url.parse(_url,true));
-      fs.readdir('./contents', function (err, filelist) {
-        var list = Template.List(filelist, filelists);
-        title = 'Node js';
-        description = "Hello Node js!";
-        body += `<p>${description}</p>`;
-        html = Template.HTML(title, list, body, '');
-        response.writeHead(200);
-        response.end(html);
-
-      });
+      db.query('SELECT * FROM TOPIC', function (err, result) {//db client에서 db server에 쿼리날리기 
+        if (err) {//error발생시
+          console.log(err.stack);
+        }
+        else {
+          var list = Template.List(result, resultlist);
+          title = 'Node js';
+          description = "Hello Node js!";
+          body += `<p>${description}</p>`;
+          html = Template.HTML(title, list, body, '');
+          response.writeHead(200);
+          response.end(html);
+        }
+      })
 
     }//홈으로 드렁왔다면
-    else {
-      console.log(url.parse(_url,true));
-      fs.readdir('./contents', function (err, filelist) {
-        var list = Template.List(filelist, filelists);
-        if (filelists.indexOf(queryData.id) == -1) {    //querystring이 없는값으로 들어오면
-          response.writeHead(404);
-          response.end('Not found');
-        } //id값이 이상한 곳으로 들어오면
-
-        else {  //id값이 있는 애들쪽으로 들어온다면
-          fs.readFile(`contents/${title}`, 'utf8', function (err, description) {
-          var sanitize_title=sanitizehtml(title);
-          var sanitize_description=sanitizehtml(description,{allowedTags:['a','h1']});
-        
-            if (err) throw err;
-            body += `<p>${sanitize_description}</p>`;
-            var control = `
-            <a href="/update?id=${sanitize_title}">update</a>
+    else {//id값으로 들어왔다면
+      db.query('SELECT * FROM TOPIC', function (err1, result) {//db client에서 db server에 쿼리날리기 
+        if (err1) {//error발생시
+          throw err1;
+        }
+        else {
+          var list = Template.List(result, resultlist);
+          if (resultlist.indexOf(queryData.id) == -1) {    //querystring이 없는값으로 들어오면
+            response.writeHead(404);
+            response.end('Not found');
+          }//id값이 이상한 곳으로 들어오면
+          else {
+            db.query(`SELECT * FROM TOPIC WHERE ID=?`,[queryData.id], function (err2, topic) {
+              if (err2) {
+                throw err2;
+              }
+              title = topic[0].title;
+              description = topic[0].description;
+              body += `<p>${description}</p>`;
+              var control = `
+              <a href="/create">create</a>
+              <a href="/update?id=${queryData.id}">update</a>
             <form action="delete_process" method="POST">
-            <input type="hidden" name="object" value="${sanitize_title}">
-            <input type="submit" value="delete" onsubmit="alert('delete?')"/>
+            <input type="hidden" name="object" value="${queryData.id}">
+            <h2><input type="submit" value="delete" onsubmit="alert('delete?')"/></h2>
             </form>
             `
-            html = Template.HTML(sanitize_title, list, body, control);
-            response.writeHead(200);
-            response.end(html);
+              html = Template.HTML(title, list, body, control);
+              response.writeHead(200);
+              response.end(html);
+            })         //id값이 db에 존재하는 title값으로 들어온다면,
           }
-          );
         }
-
-
       });
     }
+    // fs.readdir('./contents', function (err, filelist) {
+    //   if (resultlist.indexOf(queryData.id) == -1) {    //querystring이 없는값으로 들어오면
+    //     response.writeHead(404);
+    //     response.end('Not found');
+    //   } //id값이 이상한 곳으로 들어오면
+
+    //   else {  //id값이 있는 애들쪽으로 들어온다면
+    //     fs.readFile(`contents/${title}`, 'utf8', function (err, description) {
+    //       var sanitize_title = sanitizehtml(title);
+    //       var sanitize_description = sanitizehtml(description, { allowedTags: ['a', 'h1'] });
+
+    //       if (err) throw err;
+    //       body += `<p>${sanitize_description}</p>`;
+    //       var control = `
+    //       <a href="/update?id=${sanitize_title}">update</a>
+    //       <form action="delete_process" method="POST">
+    //       <input type="hidden" name="object" value="${sanitize_title}">
+    //       <input type="submit" value="delete" onsubmit="alert('delete?')"/>
+    //       </form>
+    //       `
+    //       html = Template.HTML(sanitize_title, list, body, control);
+    //       response.writeHead(200);
+    //       response.end(html);
+    //     }
+    //     );
+    //   }
   }
   else if (pathname === "/create") {
     body = `<form action="http://localhost:8088/create_process" method="POST" >
@@ -84,7 +126,7 @@ var app = http.createServer(function (request, response) {
       </p>
     </form>`;
     fs.readdir('./contents', function (err, filelist) {
-      var list = Template.List(filelist, filelists);
+      var list = Template.List(filelist, resultlist);
       title = 'WEB-create';
       html = Template.HTML(title, list, body, '');
       response.writeHead(200);
@@ -132,8 +174,10 @@ var app = http.createServer(function (request, response) {
       </p>
     </form>`;
 
-        var list = Template.List(filelist, filelists);
-        var control = `<h2><a href=/update?id=${queryData.id}>update</a></h2>`
+        var list = Template.List(filelist, resultlist);
+        var control = `
+        <a href="/create">create</a>
+        <a href=/update?id=${queryData.id}>update</a>`
         title = 'WEB-update';
         html = Template.HTML(title, list, body, control);
         response.writeHead(200);
