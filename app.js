0eaf3a261636e65bd6bd929fc27fd4392e73fddf
hyunsooklm <hyunsooklm@naver.com>
2020-05-10 22:40:49 +0900

    Express로 delete과정까지 마무리

--- app.js
+++ app.js
@@ -35,8 +35,18 @@ app.get('/page/:pageId', function (req, res) {
       if (err) throw err;
       body += `<p>${sanitize_description}</p>`;
       var control = `
-        <a href="/update?id=${sanitize_title}">update</a>
-        <form action="delete_process" method="POST">
+      <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+      <script>  
+      $(document).ready(()=>{
+          $('input[type="submit"]').on('click',(event)=>{
+            if(!(confirm("정말로 삭제하시겠습니까?"))){
+              event.preventDefault();  
+            }
+          })
+        })
+      </script>
+        <a href="/page_update/${sanitize_title}">update</a>
+        <form action="/page_delete" method="POST">
         <input type="hidden" name="object" value="${sanitize_title}">
         <input type="submit" value="delete" onsubmit="alert('delete?')"/>
         </form>
@@ -73,20 +83,93 @@ app.post('/page_create', (req, res) => {  //post로 받는거라면, app.post로
   req.on('data', (data) => {
     body += data;
   });
-  req.on('end',()=>{
-    let post=qs.parse(body);
-    let title=sanitizehtml(post.title);
-    let description=sanitizehtml(post.description);
-    fs.writeFile(`./contents/${title}`,description,'utf8',(err)=>{
-      if(err)
-      throw err;
-      else{
+  req.on('end', () => {
+    let post = qs.parse(body);
+    let title = sanitizehtml(post.title);
+    let description = sanitizehtml(post.description);
+    fs.writeFile(`./contents/${title}`, description, 'utf8', (err) => {
+      if (err)
+        throw err;
+      else {
         res.redirect(`/page/${title}`);
       }
     })
   })
 })
 
+app.get('/page_update/:pageId', (req, res) => {
+  fs.readdir('./contents', function (err1, filelist) {
+    if (err1) {
+      throw err1;
+    }
+    let filename = req.params.pageId;
+    fs.readFile(`./contents/${filename}`, 'utf8', (err2, data) => {
+      if(err2){
+        throw err2;
+      }
+      body = `<form action="/page_update" method="POST" >
+          <p><input type="text" name="title" value=${filename}></p>
+          <p>
+            <textarea name="description">${data}</textarea>
+          </p>
+          <p>
+            <input type="hidden" name="old_path" value=${filename}>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>`;
+
+      var list = Template.List(filelist, filelists);
+      title = 'WEB-update';
+      html = Template.HTML(title, list, body, '');
+
+      res.send(html);
+    });
+  });
+}
+)
+
+app.post('/page_update',(req,res)=>{
+  var body = "";
+  req.on('data', (data) => {
+    body += data;
+  });
+  req.on('end', () => {
+    let post = qs.parse(body);
+    console.log(post);
+    let title = sanitizehtml(post.title);
+    let description = sanitizehtml(post.description);
+    fs.rename(`./contents/${post.old_path}`,`./contents/title`,(err,data)=>{
+      if(err){
+        throw err;
+      }
+    });
+    fs.writeFile(`./contents/${title}`, description, 'utf8', (err) => {
+      if (err)
+        throw err;
+      else {
+        res.redirect(`/page/${title}`);
+      }
+    })
+  })
+  }
+)
+app.post('/page_delete',(req,res)=>{
+var body="";
+req.on('data',(data)=>{
+  body+=data;
+})
+req.on('end',()=>{
+  let post=qs.parse(body);
+  fs.unlink(`./contents/${post.object}`,(err)=>{
+    if(err){
+      throw err;
+    }
+    res.redirect('/');
+  })
+})
+});
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 
@@ -102,143 +185,4 @@ var Template=require('./lib/Template.js');
 var isEmpty=require('./lib/Empty.js');
 var sanitizehtml=require('sanitize-html');
 
-
-var app = http.createServer(function (request, response) {
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;    //querystring 따는법
-  var pathname = url.parse(_url, true).pathname;
-  var title = queryData.id;
-  var html = "";
-  var description = "";
-  var filelists = [];
-  var body = '';
-  console.log(queryData);
-  else if (pathname === "/create") {
-    body = `<form action="http://localhost:8088/create_process" method="POST" >
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>`;
-    fs.readdir('./contents', function (err, filelist) {
-      var list = Template.List(filelist, filelists);
-      title = 'WEB-create';
-      html = Template.HTML(title, list, body, '');
-      response.writeHead(200);
-      response.end(html);
-    });
-
-  }
-  else if (pathname === "/create_process") { //post방식으로 전송된 데이터 받기.
-    var post = {};
-    if (request.method == "POST") {
-      var body = "";
-      request.on('data', function (data) {
-        body += data;
-      });
-      request.on('end', function () {
-        post = qs.parse(body);
-        var title = post.title;
-        var description = post.description;
-        // var sanitize_title=sanitizehtml(title);
-        // var sanitize_description=sanitizehtml(description);
-        fs.writeFile(`./contents/${title}`, description, 'utf8', function (err) {
-          if (err) throw err;
-          console.log("file write success");
-          response.writeHead(302, {         //3.xx로 시작된 코드는 Redirection을 의미한다.
-            'Location': `/?id=${title}`
-            //add other headers here...
-          });
-          response.end('success');
-        })
-      });
-
-    }
-  }
-  else if (pathname === "/update") {
-    fs.readdir('./contents', function (err, filelist) {
-      fs.readFile(`contents/${title}`, 'utf8', function (err, description) {
-        var body = `<form action="/update_process" method="POST" >
-      <input type="hidden" name="object" value="${title}">
-      <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-      <p>
-        <textarea name="description" placeholder="description">${description}</textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>`;
-
-        var list = Template.List(filelist, filelists);
-        var control = `<h2><a href=/update?id=${queryData.id}>update</a></h2>`
-        title = 'WEB-update';
-        html = Template.HTML(title, list, body, control);
-        response.writeHead(200);
-        response.end(html);
-      });
-
-    });
-  }
-  else if (pathname === "/update_process") {   //파일 수정
-    var post = {};
-    if (request.method == "POST") {
-      var body = "";
-      request.on('data', function (data) {
-        body += data;
-      });
-      request.on('end', function () {
-        post = qs.parse(body);
-        title = post.title; //변경된 title
-        description = post.description; //변경된 내용
-        var object = post.object; //원본 파일
-        fs.rename(`./contents/${object}`, `./contents/${title}`, function (err) {
-          if (err) {
-            console.log(`error:${err}`);
-          }
-          console.log('rename success');
-          fs.writeFile(`./contents/${title}`, description, 'utf8', function (err) {
-            if (err) throw err;
-            response.writeHead(302, {         //3.xx로 시작된 코드는 Redirection을 의미한다.
-              'Location': `/?id=${title}`
-              //add other headers here...
-            });
-            response.end('success');
-          })
-        })
-      })
-    }
-
-  }
-  else if (pathname === "/delete_process") {
-    var post = {};
-    if (request.method == "POST") {
-      var body = "";
-      request.on('data', function (data) {
-        body += data;
-      });
-      request.on('end', function () {
-        post = qs.parse(body);
-        var object = post.object; //이녀석을 삭제하면 된다.
-        fs.unlink(`./contents/${object}`, function (err) {
-          if (err) {
-            console.log(`delete error:${err}`);
-          }
-          console.log('delete success!');
-          response.writeHead(302, { 'Location': '/' });
-          response.end();
-        })
-      })
-    }
-  }
-  else {//pathname이 /이 아닌경우
-    response.writeHead(404);
-    response.end('Not found');
-  }
-});
-
-
-app.listen(8088);
 */
