93332cb80ec949a525b84f4d0ab758e565a1691b
hyunsooklm <hyunsooklm@naver.com>
2020-05-10 21:31:43 +0900

    create과정까지 구현

--- app.js
+++ app.js
@@ -1,15 +1,91 @@
 const express = require('express')
+const Template = require('./lib/Template');
+const fs = require('fs');
 const app = express()
 const port = 3000
+const sanitizehtml = require('sanitize-html');
+const qs = require("querystring");
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/page',function(req,res){
-  return res.send('HI');
+let filelists = [];
+app.get('/', (req, res) => fs.readdir('./contents', function (err, filelist) {
+  var body = "";
+  var html = "";
+  var list = Template.List(filelist, filelists);
+  var title = 'Node js';
+  var description = "Hello Node js!";
+  body += `<p>${description}</p>`;
+  html = Template.HTML(title, list, body, '');
+  res.send(html);
 })
-app.get('/page',function(req,res){
-  return res.send('HI');
+)
+
+app.get('/page/:pageId', function (req, res) {
+  fs.readdir('./contents', function (err, filelist) {
+    var list = Template.List(filelist, filelists);
+    var body = ""
+    let title = req.params.pageId;
+    //id값이 있는 애들쪽으로 들어온다면
+    if (filelists.indexOf(title) == -1) {    //querystring이 없는값으로 들어오면
+      res.status(404).send('Sorry cant find that!');
+    } //id값이 이상한 곳으로 들어오면
+    fs.readFile(`contents/${title}`, 'utf8', function (err, description) {
+      var sanitize_title = sanitizehtml(title);
+      var sanitize_description = sanitizehtml(description, { allowedTags: ['a', 'h1'] });
+
+      if (err) throw err;
+      body += `<p>${sanitize_description}</p>`;
+      var control = `
+        <a href="/update?id=${sanitize_title}">update</a>
+        <form action="delete_process" method="POST">
+        <input type="hidden" name="object" value="${sanitize_title}">
+        <input type="submit" value="delete" onsubmit="alert('delete?')"/>
+        </form>
+        `
+      html = Template.HTML(sanitize_title, list, body, control);
+      res.send(html);
+    }
+    );
+
+  });
 }) //페이지 라우팅, path에 따라 다른 콜백함수가 실행되도록 처리.
 
+app.get('/page_create', (req, res) => {
+  body = `<form action="/page_create" method="POST" >
+  <p><input type="text" name="title" placeholder="title"></p>
+  <p>
+    <textarea name="description" placeholder="description"></textarea>
+  </p>
+  <p>
+    <input type="submit">
+  </p>
+</form>`;
+  fs.readdir('./contents', function (err, filelist) {
+    var list = Template.List(filelist, filelists);
+    title = 'WEB-create';
+    html = Template.HTML(title, list, body, '');
+    res.send(html);
+  });
+}
+)
+
+app.post('/page_create', (req, res) => {  //post로 받는거라면, app.post로 이미 받기때문에 경로를 create_process로 할 필요 없다.
+  var body = "";
+  req.on('data', (data) => {
+    body += data;
+  });
+  req.on('end',()=>{
+    let post=qs.parse(body);
+    let title=sanitizehtml(post.title);
+    let description=sanitizehtml(post.description);
+    fs.writeFile(`./contents/${title}`,description,'utf8',(err)=>{
+      if(err)
+      throw err;
+      else{
+        res.redirect(`/page/${title}`);
+      }
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
@@ -37,60 +113,6 @@ var app = http.createServer(function (request, response) {
   var filelists = [];
   var body = '';
   console.log(queryData);
-  if (pathname === '/') {
-    if (queryData.id == undefined) {
-      var path=url.parse(_url,true).path;
-      if(path!='/'){
-        response.writeHead(404);
-        response.end('Not found!');
-      }
-      console.log(url.parse(_url,true));
-      fs.readdir('./contents', function (err, filelist) {
-        var list = Template.List(filelist, filelists);
-        title = 'Node js';
-        description = "Hello Node js!";
-        body += `<p>${description}</p>`;
-        html = Template.HTML(title, list, body, '');
-        response.writeHead(200);
-        response.end(html);
-
-      });
-
-    }//홈으로 드렁왔다면
-    else {
-      console.log(url.parse(_url,true));
-      fs.readdir('./contents', function (err, filelist) {
-        var list = Template.List(filelist, filelists);
-        if (filelists.indexOf(queryData.id) == -1) {    //querystring이 없는값으로 들어오면
-          response.writeHead(404);
-          response.end('Not found');
-        } //id값이 이상한 곳으로 들어오면
-
-        else {  //id값이 있는 애들쪽으로 들어온다면
-          fs.readFile(`contents/${title}`, 'utf8', function (err, description) {
-          var sanitize_title=sanitizehtml(title);
-          var sanitize_description=sanitizehtml(description,{allowedTags:['a','h1']});
-        
-            if (err) throw err;
-            body += `<p>${sanitize_description}</p>`;
-            var control = `
-            <a href="/update?id=${sanitize_title}">update</a>
-            <form action="delete_process" method="POST">
-            <input type="hidden" name="object" value="${sanitize_title}">
-            <input type="submit" value="delete" onsubmit="alert('delete?')"/>
-            </form>
-            `
-            html = Template.HTML(sanitize_title, list, body, control);
-            response.writeHead(200);
-            response.end(html);
-          }
-          );
-        }
-
-
-      });
-    }
-  }
   else if (pathname === "/create") {
     body = `<form action="http://localhost:8088/create_process" method="POST" >
       <p><input type="text" name="title" placeholder="title"></p>
@@ -126,7 +148,7 @@ var app = http.createServer(function (request, response) {
         fs.writeFile(`./contents/${title}`, description, 'utf8', function (err) {
           if (err) throw err;
           console.log("file write success");
-          response.writeHead(302, {         //3.xx로 시작된 코드는 Redirection을 의미한다. 
+          response.writeHead(302, {         //3.xx로 시작된 코드는 Redirection을 의미한다.
             'Location': `/?id=${title}`
             //add other headers here...
           });
@@ -179,7 +201,7 @@ var app = http.createServer(function (request, response) {
           console.log('rename success');
           fs.writeFile(`./contents/${title}`, description, 'utf8', function (err) {
             if (err) throw err;
-            response.writeHead(302, {         //3.xx로 시작된 코드는 Redirection을 의미한다. 
+            response.writeHead(302, {         //3.xx로 시작된 코드는 Redirection을 의미한다.
               'Location': `/?id=${title}`
               //add other headers here...
             });
