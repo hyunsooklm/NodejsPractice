3a67705e1ae843987ad73bc039d6eaca5fb2bfa0
hyunsooklm <hyunsooklm@naver.com>
2020-05-28 17:43:00 +0900

    google+ api 인증 에러 해결해야함 accesstoken까지는 가져옴

--- auth_UI/auth_check.js
+++ auth_UI/auth_check.js
@@ -3,7 +3,10 @@ module.exports={
         return req?true:false;
     },
     auth_ui : function auth_UI(req, res) {
-        var auth_ui = `<a href="/auth/login">login</a>|<a href="/auth/register">register</a>`
+        var auth_ui = `
+        <a href="/auth/login">login</a>|
+        <a href="/auth/register">register</a>|
+        <a href="/auth/google">Google Login</a>`
         if (req.user) {
             auth_ui = `${req.user.Displayname}|<a href="/auth/logout">logout</a>`
         }
